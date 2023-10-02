075ec13b01abc218d6e55d548dd8043f62c5ead6
Sedutmir <sedutmir@yahoo.com>
2023-10-02 04:27:40 +0300

    Subscribe section

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -4,7 +4,15 @@ import ParallaxItem from "./Parallax/ParallaxItem";
 export default function Home() {
     return <>
         <ParallaxItem/>
-        {/*  Subscribe container  */}
+        <section className={"subscribe"}>
+            <div>
+                <h2>Subcribe to our Newsletter</h2>
+                <form>
+                    <input type="email" name="email" placeholder={"E-mail"} required={true}/>
+                    <input type="submit" value="Subscribe"/>
+                </form>
+            </div>
+        </section>
         {/*  Social container  */}
     </>;
 }
