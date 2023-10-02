7ec126919dd57f3f18e2264849093e891c231b73
Sedutmir <sedutmir@yahoo.com>
2023-10-02 05:17:05 +0300

    Socials section

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,6 +1,12 @@
 import "./Home.css";
 import ParallaxItem from "./Parallax/ParallaxItem";
 
+import Discord from "../../assets/discord.svg";
+import Telegram from "../../assets/telegram.svg";
+import VK from "../../assets/vk.svg";
+import Twitch from "../../assets/twitch.svg";
+import GitHub from "../../assets/github.svg";
+
 export default function Home() {
     return <>
         <ParallaxItem/>
@@ -13,6 +19,21 @@ export default function Home() {
                 </form>
             </div>
         </section>
-        {/*  Social container  */}
+        <section className="social">
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/K3GwvMvzBTY?si=jXvBjBUg7DXCyhOu" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <div class={"social-text"}>
+                    <p><b>The North</b> - проект, зародившийся в студенческие времена!</p>
+                    <p>Мы поставили себе цель продвинуть игростроительство в Московском Политехе и создали <b>крупную</b> и <b>интересную</b> игру про <b>викингов</b>. </p>
+                </div>
+                <ul className="social-icons">
+                    <li><a href="#"><img src={Discord} alt=""/></a></li>
+                    <li><a href="#"><img src={Telegram} alt=""/></a></li>
+                    <li><a href="#"><img src={VK} alt=""/></a></li>
+                    <li><a href="#"><img src={Twitch} alt=""/></a></li>
+                    <li><a href="#"><img src={GitHub} alt=""/></a></li>
+                </ul>
+            </div>
+        </section>
     </>;
 }
